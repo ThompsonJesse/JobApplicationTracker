55796a5e93ff8971ae1a93b93698548ff6ba30aa
Jesse Thompson <thompsonjessea@gmail.com>
2018-03-07 01:11:21 -0600

    Moved key and client_id out of manifest.json

--- tasks/manifest.js
+++ tasks/manifest.js
@@ -1,23 +1,32 @@
-import gulp from 'gulp'
-import { paths } from '../gulpfile'
-import jeditor from 'gulp-json-editor'
-import gulpif from 'gulp-if'
-
+import gulp from "gulp";
+import { paths } from "../gulpfile";
+import jeditor from "gulp-json-editor";
+import gulpif from "gulp-if";
+import merge from "gulp-merge-json";
 
 function addAutoreloadScript(manifestJson) {
-  const hasScripts = manifestJson.background && manifestJson.background.scripts
+  const hasScripts = manifestJson.background && manifestJson.background.scripts;
 
   return {
     ...manifestJson,
     background: {
       ...manifestJson.background,
-      scripts: [ 'autoreload.js', ...(hasScripts ? manifestJson.background.scripts : []) ],
-    },
-  }
+      scripts: [
+        "autoreload.js",
+        ...(hasScripts ? manifestJson.background.scripts : [])
+      ]
+    }
+  };
 }
 
 export function manifest() {
-  return gulp.src(paths.manifest)
+  return gulp
+    .src(paths.manifest)
     .pipe(gulpif(!global.IS_PRODUCTION, jeditor(addAutoreloadScript)))
-    .pipe(gulp.dest('build'))
+    .pipe(
+      merge({
+        fileName: "manifest.json"
+      })
+    )
+    .pipe(gulp.dest("build"));
 }
